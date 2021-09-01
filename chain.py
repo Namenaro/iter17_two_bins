c6f260d2f114ae9de8c878e168a0ea46d399b743
neuro <nanenaro@gmail.com>
2021-09-01 20:17:50 +0300

    draft in progess..

---
+++ chain.py
@@ -0,0 +1,6 @@
+class Chain:
+    def __init__(self,Abin, Bbin):
+        pass
+
+    def apply2(self,pic, x,y):
+        pass
--- main.py
+++ main.py
@@ -4,12 +4,15 @@
 
 """
 from data import *
+from sampling import *
 from clicker import *
+from chain import *
 from measure_prediction_error import *
 from logger import *
 from utils import *
 from binary import *
 from nonbinary import *
+
 
 def exp0(logger):
     A_sens_rad = 0
@@ -25,25 +28,33 @@ def exp0(logger):
     B_event_diam_grid = range(0, 700, step)
 
     etalon_pic = etalons_of3()[0]  # эталон
-    pics_train = get_numbers_of_type(3)[0:50]  # генерация гипотезы
-    pics_test = get_numbers_of_type(3)[50:160]  # проверка гипотезы
 
     X, Y = select_coord_on_pic(etalon_pic)  # click - 0)A, 1)B, 2)h
-    A_etalon = make_measurement(etalon_pic,X[0],Y[0],A_sens_rad)
-    A = BinaryUnit(A_u_rad, A_sens_rad, A_etalon, A_event_diam,dx=0,dy=0)
-
+    A_etalon = make_measurement(etalon_pic, X[0], Y[0], A_sens_rad)
     h_etalon = make_measurement(etalon_pic, X[2], Y[2], h_sens_rad)
-    ha = NonBinaryUnit(h_u_rad, h_sens_rad,h_etalon,dx=X[2]-X[0], dy=Y[2]-Y[0])
-    hb = NonBinaryUnit(h_u_rad, h_sens_rad,h_etalon,dx=X[1]-X[0], dy=Y[1]-Y[0])
-
     B_etalon = make_measurement(etalon_pic, X[1], Y[1], B_sens_rad)
+
+    A = BinaryUnit(A_u_rad, A_sens_rad, A_etalon, A_event_diam, dx=0, dy=0)
+    ha = NonBinaryUnit(h_u_rad, h_sens_rad, h_etalon, dx=X[2] - X[0], dy=Y[2] - Y[0])
+    hb = NonBinaryUnit(h_u_rad, h_sens_rad, h_etalon, dx=X[1] - X[0], dy=Y[1] - Y[0])
+
     hA_err = []
     hB_err = []
     hAB_err = []
 
+    dataset = get_numbers_of_type(3)
+    n = len(dataset)
+    trainset_len = int(n / 2)
+    pics_train = get_numbers_of_type(3)[0:trainset_len]
+    pics_test = get_numbers_of_type(3)[trainset_len + 1:n - 1]
+    sample_size_train = 80
+    sample_size_test = 250
 
     for B_diam in B_event_diam_grid:
-        B = BinaryUnit(B_u_rad,B_sens_rad,B_etalon,B_diam,dx=X[1]-X[0], dy=Y[1]-Y[0])
+        B = BinaryUnit(B_u_rad, B_sens_rad, B_etalon, B_diam, dx=X[1] - X[0], dy=Y[1] - Y[0])
+        AB = Chain(A, B)
+        reality = make_sample_of_appliable(appliable=ha, sample_size=sample_size_test, pics=pics_test,
+                                           condition=AB.apply2)
 
 
 
